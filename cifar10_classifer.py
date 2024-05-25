35575386458ba6e9b1aea78cd2d24b2c7f712d67
Oussama Eljaafari <152564987+Ossamajaafari@users.noreply.github.com>
2024-05-25 13:43:41 +0100

    Update cifar10_classifer.py

--- cifar10_classifer.py
+++ cifar10_classifer.py
@@ -10,7 +10,7 @@ class_name = ["airplane", "automobile" , "bird" , "cat" , "deer" , "dog" , "frog
 # Create a function to load my saved model
 @st.cache_resource
 def load_my_model():
-    model = tf.keras.models.load_model("my_model.h5")
+    model = tf.keras.models.load_model("my_model_modify.h5")
     return model
 
 model = load_my_model()
